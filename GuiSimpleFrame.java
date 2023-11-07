d4dfe3fc1c484c6e9271a1faa6deef4dc54ceaaf
b191995 <110626526+b191995@users.noreply.github.com>
2023-11-07 16:51:15 +0530

    Update GuiSimpleFrame.java

--- GuiSimpleFrame.java
+++ GuiSimpleFrame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 public class GuiSimpleFrame{
 	public static void main(String args[]){
 		JFrame frame=new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
 }
